b9c0d078fba4a91dd9b71bf088d457ad1e453447
Tyler C Smith <tylercsmith94@gmail.com>
2020-12-18 20:58:21 +0700

    Add pokedex component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Pokecard from './Pokecard';
+import Pokedex from './Pokedex';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <Pokecard id={4} name="Charmander" type="fire" exp={62} />
+        <Pokedex />
       </div>
     );
 
@@ -14,15 +14,6 @@ class App extends Component {
 }
 
 
-// [
-//   {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
-//   {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
-//   {id: 11, name: 'Metapod', type: 'bug', base_experience: 72},
-//   {id: 12, name: 'Butterfree', type: 'flying', base_experience: 178},
-//   {id: 25, name: 'Pikachu', type: 'electric', base_experience: 112},
-//   {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
-//   {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
-//   {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
-// ]
+
 
 export default App;
