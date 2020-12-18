65df463efec68aab28b6c84386c545fe3198cf02
Tyler C Smith <tylercsmith94@gmail.com>
2020-12-18 21:53:46 +0700

    Styles Pokecard with animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import Pokedex from './Pokedex';
+import Pokegame from './Pokegame'
+
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <Pokedex />
+        <Pokegame />
       </div>
     );
 
--- src/Pokecard.js
+++ src/Pokecard.js
@@ -13,7 +13,10 @@ class Pokecard extends Component {
     return (
       <div className="Pokecard">
         <h1 className="Pokecard-title">{this.props.name}</h1>
+        
+        <div className="Pokecard-img">
         <img src={imgSrc} alt={this.props.name} />
+        </div>
         <div className="Pokecard-data">Type: {this.props.type}</div>
         <div className="Pokecard-data">Exp: {this.props.exp}</div>
       </div>
